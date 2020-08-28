139ad319cfaec41f843a691c7cbe5b93f99ae588
ahaydarda <a.haydar.acar@gmail.com>
2020-08-28 17:33:11 +0100

    first commit: set up with nextjs, tailwind css, tailwind ui

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+// pages/index.jsx
+import Head from 'next/head'
+import {Ready} from './ready';
+
+const Home = () => (
+    <div>
+      <Head>
+        <title>Next.js TailwindCSS</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+      </Head>
+      
+      <div className="container mx-auto">
+        <h1 className="text-lg text-center m-4">TailwindUI/Next.js Blog</h1>
+        <p className="bg-green-600">This is a test of the tailwind works.</p>
+        <p className="bg-green-600">Below is test that tailwind UI works </p>
+         <Ready />
+      </div>
+    </div>
+)
+
+export default Home
